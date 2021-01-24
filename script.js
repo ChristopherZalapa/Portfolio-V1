7acd3918f16aa9d1a10fffa9c140f9a5d5313e89
ChristopherZalapa <chriszalapa@gmail.com>
2021-01-23 23:52:38 -0800

    Splash Fade In & Out Working

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ let headerText = document.querySelectorAll('.header-text');
 window.addEventListener('DOMContentLoaded', () => {
 
     setTimeout(() => {
+        
         headerText.forEach((span, idx) => {
             setTimeout(() => {
                 span.classList.add('active');
+            }, (idx + 1) * 500)
+        });
+
+        setTimeout(() => {
+            headerText.forEach((span, idx) => {
+                
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 100)
             })
-        })
+        }, 2000)
+
+        setTimeout(() => {
+            splash.style.top = '-100vh'
+        }, 4500)
     })
 });
 // --------- End Of Splash --------- 
